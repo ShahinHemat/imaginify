359a3b0596a8ad41cca80bcfa578d2849f1ddcc5
Shahin Endregard Hemat <shahinhemat@gmail.com>
2024-02-18 00:23:13 +0100

    Update MediaUploader.tsx

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -29,7 +29,7 @@ const MediaUploader = ({
       publicId: result?.info?.public_id,
       width: result?.info?.width,
       height: result?.info?.height,
-      secureURL: result?.info?.secure_URL
+      secureURL: result?.info?.secure_url //URL
     }))
 
     onValueChange(result?.info?.public_id)
